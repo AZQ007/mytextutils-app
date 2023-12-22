9d5ca36dacb4c9c1687f847b9a59694022025eea
AZQ007 <ahmadzaheer7991@gmail.com>
2023-12-22 10:47:23 +0000

    My first React App

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,6 +31,7 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text is Copied!","success");
 
     }
@@ -40,6 +41,12 @@ export default function TextForm(props) {
         props.showAlert("Removed extra spaces!","success");
 
     }
+
+    const handleClearText = ()=>{
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!","success");
+    }
     
 
     const [text, setText] = useState('');
@@ -47,15 +54,16 @@ export default function TextForm(props) {
     return (
         <>
         <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
-            <h1>{props.heading}</h1>
+            <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#13466e':'white',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={textToSpeech}>Text to Speech</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={textToSpeech}>Text to Speech</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearText}>Clear Text</button>
 
 
 
@@ -64,10 +72,11 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p>{text.trim() === '' ? 0 : text.match(/\S+/g).length} words and {text.replace(/\s+/g, '').length} characters</p>            <p>{0.008 * text.split(" ").length} Total minutes to read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.replace(/\s+/g, '').length} characters</p>          
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Total minutes to read</p>
             <p>Total number of sentences is {text.split(".").length-1}</p>
             <h2 my-3>Preview</h2>
-            <p>{text.length>0?text:"Enter something in textbox above to preview it here"}</p>
+            <p>{text.length>0?text:"Nothing to Preview!"}</p>
         </div>
         </>
   )
